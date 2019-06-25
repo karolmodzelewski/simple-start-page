eab812b3123f8d15817919b9cc43a628645394b6
karolmodzelewski <kar.modzelewski@gmail.com>
2019-06-25 11:45:44 +0200

    Move file to new directory

---
+++ assets/js/clock.js
@@ -0,0 +1,17 @@
+var addZero = function(i) 
+{
+    return ( i < 10 ) ? "0" + i  :  i;
+}
+
+function clock()
+{
+    var date = new Date;
+    var hours = addZero(date.getHours());
+    var minutes = addZero(date.getMinutes());
+    var seconds = addZero(date.getSeconds());
+
+    document.querySelector("#header-clock").innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+setInterval(clock, 1000);
+clock();
