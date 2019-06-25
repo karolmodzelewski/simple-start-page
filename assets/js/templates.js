5cb5a2cc41591fa5996a74896afc28840d3baa35
karolmodzelewski <kar.modzelewski@gmail.com>
2019-06-25 15:52:20 +0200

    Initial commit

---
+++ assets/js/templates.js
@@ -0,0 +1,36 @@
+var template = getCookie("template");
+
+if(!template) 
+{
+  templateOne();
+} 
+else if(getCookie("template") == "grey")
+{
+    templateTwo();
+}
+else if(getCookie("template") == "green")
+{
+    templateThree();
+}
+else
+{
+templateOne();
+}
+
+function templateOne() 
+{
+  setCookie("template", "purple", 30);
+  document.getElementById("defaultTemplate").setAttribute("href", "assets/css/purple_theme.css");
+}
+
+function templateTwo() 
+{
+  setCookie("template", "grey", 30);
+  document.getElementById("defaultTemplate").setAttribute("href", "assets/css/grey_theme.css");
+}
+
+function templateThree() 
+{
+  setCookie("template", "green", 30);
+  document.getElementById("defaultTemplate").setAttribute("href", "assets/css/green_theme.css");
+}
