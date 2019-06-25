b88cb5935d3b41c16e7312d83ef54f2565a04ca7
karolmodzelewski <kar.modzelewski@gmail.com>
2019-06-25 11:40:24 +0200

    Initial commit

---
+++ assets/js/languages.js
@@ -0,0 +1,24 @@
+var translations = 
+{
+  "polish": 
+  {
+    "polishButton": "Polski",
+    "englishButton": "Angielski",
+    "current_hour": "Godzina:",
+    "media": "Media społecznościowe",
+    "entertainment": "Rozrywka",
+    "other": "Inne",
+    "main": "Główne"
+  },
+
+  "english": 
+  {
+    "polishButton": "Polish",
+    "englishButton": "English",
+    "current_hour": "Current hour:",
+    "media": "Social Media",
+    "entertainment": "Entertainment",
+    "other": "Other",
+    "main": "Main"
+  }
+}
