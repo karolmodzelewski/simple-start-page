e2453796a1d89a02851d88edc7b4c60f343fd8df
karolmodzelewski <kar.modzelewski@gmail.com>
2019-08-10 01:32:27 +0200

    Code optimization

--- assets/js/templates.js
+++ assets/js/templates.js
@@ -20,17 +20,17 @@ templateOne();
 function templateOne() 
 {
   setCookie("template", "purple", 30);
-  document.getElementById("defaultTemplate").setAttribute("href", "assets/css/purple_theme.css");
+  document.querySelector("#defaultTemplate").setAttribute("href", "assets/css/purple_theme.css");
 }
 
 function templateTwo() 
 {
   setCookie("template", "grey", 30);
-  document.getElementById("defaultTemplate").setAttribute("href", "assets/css/grey_theme.css");
+  document.querySelector("#defaultTemplate").setAttribute("href", "assets/css/grey_theme.css");
 }
 
 function templateThree() 
 {
   setCookie("template", "green", 30);
-  document.getElementById("defaultTemplate").setAttribute("href", "assets/css/green_theme.css");
+  document.querySelector("#defaultTemplate").setAttribute("href", "assets/css/green_theme.css");
 }
